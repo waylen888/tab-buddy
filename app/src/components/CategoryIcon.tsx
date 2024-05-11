3301e914a4a2ee3f83f7ee3734f4f7e61ef8bea2
FanChiangShihWei <waylen888@gmail.com>
2024-05-11 18:56:59 +0800

    more icons

--- app/src/components/CategoryIcon.tsx
+++ app/src/components/CategoryIcon.tsx
@@ -10,17 +10,52 @@ import LocalParkingIcon from '@mui/icons-material/LocalParking';
 import LocalTaxiIcon from '@mui/icons-material/LocalTaxi';
 import FlightIcon from '@mui/icons-material/Flight';
 import CommuteIcon from '@mui/icons-material/Commute';
+
+import RestaurantIcon from '@mui/icons-material/Restaurant';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import LiquorIcon from '@mui/icons-material/Liquor';
+import TapasIcon from '@mui/icons-material/Tapas';
+
+import ChildCareIcon from '@mui/icons-material/ChildCare';
+import CheckroomIcon from '@mui/icons-material/Checkroom';
+import SchoolIcon from '@mui/icons-material/School';
+import RedeemIcon from '@mui/icons-material/Redeem';
+import ReceiptLongIcon from '@mui/icons-material/ReceiptLong';
+import VaccinesIcon from '@mui/icons-material/Vaccines';
+import CalculateIcon from '@mui/icons-material/Calculate';
+import SupportIcon from '@mui/icons-material/Support';
+
+
+import SportsEsportsIcon from '@mui/icons-material/SportsEsports';
+import TheaterComedyIcon from '@mui/icons-material/TheaterComedy';
+import AudiotrackIcon from '@mui/icons-material/Audiotrack';
+import SportsFootballIcon from '@mui/icons-material/SportsFootball';
+import ConfirmationNumberIcon from '@mui/icons-material/ConfirmationNumber';
+
 export interface Category {
   key: string;
   name: string;
   icon: React.ReactNode;
 }
+
 const transportationColor = "#996666"
+const foodAndDrinkColor = "#669966"
+const lifeColor = "#666699"
+
 export const CATEGORIES = [
   {
     title: "Uncategorized",
     subCategories: [
       { key: "", name: "General", icon: <ReceiptSharpIcon htmlColor="gray" /> },
+    ],
+  },
+  {
+    title: "Food and drink",
+    subCategories: [
+      { key: "dining_out", name: "Dining out", icon: <RestaurantIcon htmlColor={foodAndDrinkColor} /> },
+      { key: "groceries", name: "Groceries", icon: <ShoppingCartIcon htmlColor={foodAndDrinkColor} /> },
+      { key: "liquor", name: "Liquor", icon: <LiquorIcon htmlColor={foodAndDrinkColor} /> },
+      { key: "food_drink_other", name: "Other", icon: <TapasIcon htmlColor={foodAndDrinkColor} /> },
     ],
   },
   {
@@ -34,9 +69,32 @@ export const CATEGORIES = [
       { key: "parking", name: "Parking", icon: <LocalParkingIcon htmlColor={transportationColor} /> },
       { key: "plane", name: "Plane", icon: <FlightIcon htmlColor={transportationColor} /> },
       { key: "taxi", name: "Taxi", icon: <LocalTaxiIcon htmlColor={transportationColor} /> },
-      { key: "other", name: "Other", icon: <CommuteIcon htmlColor={transportationColor} /> },
+      { key: "transportation_other", name: "Other", icon: <CommuteIcon htmlColor={transportationColor} /> },
     ],
   },
+  {
+    title: "Entertainment",
+    subCategories: [
+      { key: "games", name: "Games", icon: <SportsEsportsIcon htmlColor={foodAndDrinkColor} /> },
+      { key: "movies", name: "Movies", icon: <TheaterComedyIcon htmlColor={foodAndDrinkColor} /> },
+      { key: "music", name: "Music", icon: <AudiotrackIcon htmlColor={foodAndDrinkColor} /> },
+      { key: "sports", name: "Sports", icon: <SportsFootballIcon htmlColor={foodAndDrinkColor} /> },
+      { key: "entertainment_other", name: "Other", icon: <ConfirmationNumberIcon htmlColor={foodAndDrinkColor} /> },
+    ],
+  },
+  {
+    title: "Life",
+    subCategories: [
+      { key: "childcare", name: "Childcare", icon: <ChildCareIcon htmlColor={lifeColor} /> },
+      { key: "clothing", name: "Clothing", icon: <CheckroomIcon htmlColor={lifeColor} /> },
+      { key: "education", name: "Education", icon: <SchoolIcon htmlColor={lifeColor} /> },
+      { key: "gifts", name: "Gifts", icon: <RedeemIcon htmlColor={lifeColor} /> },
+      { key: "insurance", name: "Insurance", icon: <ReceiptLongIcon htmlColor={lifeColor} /> },
+      { key: "medical_expenses", name: "Medical expenses", icon: <VaccinesIcon htmlColor={lifeColor} /> },
+      { key: "taxes", name: "Taxes", icon: <CalculateIcon htmlColor={lifeColor} /> },
+      { key: "life_other", name: "Other", icon: <SupportIcon htmlColor={lifeColor} /> },
+    ],
+  }
 ]
 export const getCategoryGroup = (key: string) => {
   const group = CATEGORIES.find(category => category.subCategories.find(subCategory => subCategory.key === key))
