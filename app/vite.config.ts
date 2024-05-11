973a22ed3aa2c5f97f27bd06903087091e25c7f6
FanChiangShihWei <waylen888@gmail.com>
2024-05-11 16:20:15 +0800

    remove pwa temp

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -6,35 +6,35 @@ import { VitePWA } from 'vite-plugin-pwa'
 export default defineConfig({
   plugins: [
     react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      manifest: {
-        name: "TAB BUDDY",
-        short_name: "TABBUD",
-        description: "A simple expense tracker",
-        // background_color: "#08F",
-        theme_color: "#08F",
-        icons: [
-          {
-            src: "./public/android-chrome-192x192.png",
-            sizes: "192x192",
-            type: "image/png",
-          },
-          {
-            src: "./public/android-chrome-512x512.png",
-            sizes: "192x192",
-            type: "image/png",
-            purpose: "any",
-          },
-          {
-            src: "./public/android-chrome-512x512.png",
-            sizes: "192x192",
-            type: "image/png",
-            purpose: "maskable",
-          },
-        ],
-      },
-    }),
+    // VitePWA({
+    //   registerType: 'autoUpdate',
+    //   manifest: {
+    //     name: "TAB BUDDY",
+    //     short_name: "TABBUD",
+    //     description: "A simple expense tracker",
+    //     // background_color: "#08F",
+    //     theme_color: "#08F",
+    //     icons: [
+    //       {
+    //         src: "./public/android-chrome-192x192.png",
+    //         sizes: "192x192",
+    //         type: "image/png",
+    //       },
+    //       {
+    //         src: "./public/android-chrome-512x512.png",
+    //         sizes: "192x192",
+    //         type: "image/png",
+    //         purpose: "any",
+    //       },
+    //       {
+    //         src: "./public/android-chrome-512x512.png",
+    //         sizes: "192x192",
+    //         type: "image/png",
+    //         purpose: "maskable",
+    //       },
+    //     ],
+    //   },
+    // }),
   ],
   server: {
     port: 3000,
